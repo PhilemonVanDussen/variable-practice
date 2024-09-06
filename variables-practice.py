180e25f59247ba164b2e2773b601807fcf92abf7
PhilemonVanDussen <vandussenph709@northwested.org>
2024-09-06 13:25:16 -0400

    Update variables-practice.py

--- variables-practice.py
+++ variables-practice.py
@@ -19,7 +19,11 @@
 # Define a variable called greeting and assign it a string value you'd use to greet someone (Good morning, Good afternoon, etc.)
 # Use the print function to print the value of one of three variables you created in Part 1
 school = 'Career Tech'
-
+favorite_singer = 'linkin park'
+greeting = Hi
+print(school)
+print(favorite_singer)
+print(greeting) 
 
 
 # Part 2 / Working with Numbers (Numeric Data)
